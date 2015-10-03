c237c799df6189931edb9bb820c7869c22687f5e
Sumit Kumar <sumi.yadav@gmail.com>
2015-10-04 03:54:48 +0530

    Initial setup

---
+++ tests/GatewayTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Omnipay\Paytm;
+
+use Omnipay\Tests\GatewayTestCase;
+
+class GatewayTest extends GatewayTestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->gateway = new Gateway($this->getHttpClient(), $this->getHttpRequest());
+
+        $this->options = array(
+            'amount' => '10.00',
+        );
+    }
+
+    public function testPurchase()
+    {
+        $response = $this->gateway->purchase($this->options)->send();
+
+        $this->assertFalse($response->isSuccessful());
+        $this->assertTrue($response->isRedirect());
+        $this->assertNull($response->getTransactionReference());
+        // $this->assertContains('https://pguat.paytm.com/oltp-web/processTransaction', $response->getRedirectUrl());
+    }
+}
